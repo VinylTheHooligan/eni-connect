ae8ad876923c74c13adec86f75743d9da37bc64b
VinylTheDuster <william.salembien@outlook.com>
2026-03-03 16:42:12 +0100

    ITER1 - suppression de symfony flex, retour a la normal

---
+++ src/DataFixtures/CampusFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Campus;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CampusFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 15; $i++)
+        {
+            $campus = new Campus();
+
+            $campus->setNom('Campus de ');
+        }
+
+        $manager->flush();
+    }
+}
