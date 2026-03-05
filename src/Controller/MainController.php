da5c79b28fc2bf06a740d91412ea4fa700c219ba
Réda Touzani <hrmonicmusic@gmail.com>
2026-03-05 10:00:42 +0100

    ITER1 - Redirection page d'accueil en fonction du statut de connexion

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,7 +11,9 @@ final class MainController extends AbstractController
     #[Route('', name: 'app_home')]
     public function index(): Response
     {
-        // Page d'accueil : redirection vers la liste des sorties
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
         return $this->redirectToRoute('outing_list');
     }
 }
